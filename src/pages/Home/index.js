967ad3131c260ae09446b98cc9d69688d0d88c3a
Franco Delgado <francodevgado@gmail.com>
2023-04-20 20:18:58 -0300

    first commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import './styles.css'
-import { Link } from 'wouter'
+import { Link, useLocation } from 'wouter'
 import SearchBar from 'components/SearchBar/SearchBar'
 import ListOfGif from 'components/ListOfGifs/ListOfGifs'
 import TrendingSearches from 'components/TrendingSearches/TrendingSearches'
 import useGifs from 'hooks/useGifs'
-import { useLocation } from "wouter"
 import { useCallback } from 'react'
 
 export default function Home(){
@@ -29,51 +27,3 @@ export default function Home(){
         </main>
     )
 }
-=======
-import { useLocation } from 'wouter'
-import { XAlignDiv, MainContainer } from 'components/styled/Containers' 
-import { useState } from 'react'
-import useGifs from 'hooks/useGifs'
-import ListOfGifs from 'components/ListOfGifs'
-import { StyledButton, StyledInput, StyledForm } from 'components/styled/Inputs'
-import TrendingSearches from 'components/TrendingSearches'
-import { Subtitle } from 'components/styled/Headings'
-import Spinner from 'components/Spinner'
-
-const Home = () => {
-    const [keyword, setKeyword] = useState('')
-    const [path, pushLocation] = useLocation()
-    const { loading, gifs } = useGifs()
-    const lastSearchTitle = localStorage.getItem('lastKeyword')
-
-    const handleSubmit = (evt) => {
-        evt.preventDefault()
-        if(keyword !== ''){
-            pushLocation(`/search/${keyword}`)
-        }
-    }
-
-    const handleChange = evt => {
-        setKeyword(evt.target.value)
-    }
-
-    return (
-        <>
-          <XAlignDiv>
-                <StyledForm onSubmit={handleSubmit} >
-                  <StyledInput search placeholder="Busca tus gifs aquí" onChange={handleChange} type="text" value={keyword} />
-                  <StyledButton>Buscar</StyledButton>
-                </StyledForm>
-                  <Subtitle left topMargin>↓ Tu última búsqueda: {decodeURI(lastSearchTitle)} </Subtitle>
-                <MainContainer>
-                  {loading ? <Spinner/>
-                  : <ListOfGifs gifs={gifs} />}
-                  <TrendingSearches />
-                </MainContainer>
-          </XAlignDiv>
-        </>
-    )
-}
-
-export default Home
->>>>>>> 385d19605c32d7398ce4edc2573bb03b74ac926a
